932ed0249d6b784b0e11cd2a16461f04162184f6
Jose <jmejiamu@gmail.com>
2021-05-09 15:25:31 -0700

    added create table scrip

---
+++ DB/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS login (
+	id SERIAL PRIMARY KEY,
+	hash TEXT ,
+	email TEXT,
+);
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	name TEXT ,
+	email TEXT,
+  picture TEXT
+);
