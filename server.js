c3790f89be69055a902c89c5ff24a7a142b8bd95
Jose <jmejiamu@gmail.com>
2021-05-09 17:07:37 -0700

    added bcryp

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const knex = require('knex');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 
 
 // Config ENV 
@@ -21,6 +22,46 @@ const db = knex({
         database: process.env.DATABASE,
     }
 });
+
+app.post('/register', validinfo, async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const userExist = await db.select('email').from('login').where({ email: email })
+
+        if (userExist.length > 0) {
+            return res.json({ response: "User already exist" })
+        }
+        register.handleRegister(req, res, db, bcrypt)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({ response: "error" })
+
+    }
+})
+
+app.post('/signin', validinfo, signin.handleSignin(db, bcrypt))
+
+app.get('/isverify', authorization, (req, res) => {
+    try {
+        res.json(true)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ response: "Server Error" })
+    }
+})
+
+app.get('/data', authorization, async (req, res) => {
+    try {
+        const user = await db.select('id', 'name', 'picture').from('users').where({ id: req.user })
+        // console.log(user);
+        res.json(user[0])
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ response: 'Server Error' })
+    }
+})
 
 
 app.listen(3001, process.env.URL, () => {
